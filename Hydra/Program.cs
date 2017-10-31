0dbd4123e3b9ed1eeab64d41144c222e5fcf831b
wallaceiam <chris@mymojo.ca>
2017-10-31 22:41:41 +0000

    Add project files.

---
+++ Hydra/Program.cs
@@ -0,0 +1,89 @@
+﻿using Hydra.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hydra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repo = new KrakenExchange();
+
+            var result = repo.GetTradableAssetPairsAsync().Result;
+
+            // get all possibilities
+            var possibilities = new List<string>();
+            foreach (var key in result.Result.Keys)
+            {
+                if (result.Result[key].Base.StartsWith("Z") || result.Result[key].Quote.StartsWith("Z"))
+                {
+                    if (!key.EndsWith(".d"))
+                        possibilities.Add(key);
+                }
+            }
+
+            var paths = new Dictionary<string, object>();
+            foreach (var pair in possibilities)
+            {
+                if (pair.Contains("EUR"))
+                {
+                    var ab = pair;
+                    var aquote = result.Result[ab].Quote;
+                    var des = possibilities.Where(x => x != ab && result.Result[x].Quote == aquote);
+                    foreach (var de in des)
+                    {
+                        foreach (var bc in possibilities.Where(x => x != ab && x != de && result.Result[x].Base == result.Result[ab].Base && result.Result[x].Quote != aquote))
+                        {
+                            foreach (var cd in possibilities.Where(x => x != ab && x != de && x != bc))
+                            { 
+                                if (result.Result[cd].Base == result.Result[de].Base &&
+                                    result.Result[bc].Quote == result.Result[cd].Quote &&
+                                    result.Result[bc].Base != result.Result[cd].Base)
+                                {
+                                    paths.Add(ab + "|" + bc + "|" + cd + "|" + de, null);
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            var priceData = repo.GetTickerInformationAsync(possibilities).Result;
+
+            // work out each path
+
+            foreach(var path in paths.Keys)
+            {
+                var starting = 100.0M;
+                var parts = path.Split('|');
+                var ab = parts[0];
+                var bc = parts[1];
+                var cd = parts[2];
+                var de = parts[3];
+
+                //var abPrice = priceData.Result[ab].Ask[0];
+                //var bcPrice = priceData.Result[bc].Bid[0];
+                //var cdPrice = priceData.Result[cd].Ask[0];
+                //var dePrice = priceData.Result[bc].Bid[0];
+                var abPrice = priceData.Result[ab].Last[0];
+                var bcPrice = priceData.Result[bc].Last[0];
+                var cdPrice = priceData.Result[cd].Last[0];
+                var dePrice = priceData.Result[de].Last[0];
+
+                var abAmount = starting / abPrice;
+                var bcAmount = abAmount * bcPrice;
+                var cdAmount = bcAmount / cdPrice;
+                var ending = cdAmount * dePrice;
+
+                var perc = ((ending - starting) / starting);
+                if(perc > 0.05M)
+                    Console.WriteLine($"{path}\t\t{ending}\t{perc:P2}");
+            }
+            
+            Console.Read();
+        }
+    }
+}
---
+++ Hydra/Repositories/KrakenExchange.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hydra.Repositories
+{
+    public class KrakenExchange
+    {
+        //  https://api.kraken.com/0/public/AssetPairs
+        public async Task<AssetPairs> GetTradableAssetPairsAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+                client.DefaultRequestHeaders.Add("User-Agent", ".Hyrda");
+
+                var settings = new DataContractJsonSerializerSettings()
+                {
+                    UseSimpleDictionaryFormat = true
+                };
+
+                var serializer = new DataContractJsonSerializer(typeof(AssetPairs), settings);
+
+                var streamTask = client.GetStreamAsync("https://api.kraken.com/0/public/AssetPairs");
+                var repositories = serializer.ReadObject(await streamTask) as AssetPairs;
+
+                return repositories;
+            }
+        }
+
+        public async Task<TickerInformation> GetTickerInformationAsync(List<string> assetPairs)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+                client.DefaultRequestHeaders.Add("User-Agent", ".Hyrda");
+
+                var settings = new DataContractJsonSerializerSettings()
+                {
+                    UseSimpleDictionaryFormat = true
+                };
+
+                var serializer = new DataContractJsonSerializer(typeof(TickerInformation), settings);
+
+                var streamTask = client.GetStreamAsync("https://api.kraken.com/0/public/Ticker?pair=" + string.Join(",", assetPairs));
+                var repositories = serializer.ReadObject(await streamTask) as TickerInformation;
+
+                return repositories;
+            }
+        }
+    }
+
+    [DataContract]
+    public class AssetPairs
+    {
+        //public object error { get; set; }
+        [DataMember(Name = "result")]
+        public Dictionary<string, AssetPair> Result { get; set; }
+    }
+
+    [DataContract]
+    public class TickerInformation
+    {
+        [DataMember(Name = "result")]
+        public Dictionary<string, Ticker> Result { get; set; }
+    }
+
+    [DataContract]
+    public class AssetPair
+    {
+        [DataMember(Name = "altname")]
+        public string AltName { get; set; }
+        [DataMember(Name = "aclass_base")]
+        public string AClassBase { get; set; }
+        [DataMember(Name = "base")]
+        public string Base { get; set; }
+        [DataMember(Name = "aclass_quote")]
+        public string AClassQuote { get; set; }
+        [DataMember(Name = "quote")]
+        public string Quote { get; set; }
+        public string lot { get; set; }
+        public decimal air_decimals { get; set; }
+        public decimal lot_decimals { get; set; }
+        public decimal lot_multiplier { get; set; }
+        //"leverage_buy":[
+
+        //],
+        //"leverage_sell":[
+
+        //],
+        //"fees":[  ],
+        //"fees_maker":[  ],
+        public string fee_volume_currency { get; set; }
+        public decimal margin_call { get; set; }
+        public decimal margin_stop { get; set; }
+    }
+
+    [DataContract]
+    public class Ticker
+    {
+        [DataMember(Name = "a")]
+        public List<decimal> Ask { get; set; }
+        [DataMember(Name = "b")]
+        public List<decimal> Bid { get; set; }
+
+        [DataMember(Name ="c")]
+        public List<decimal> Last { get; set; }
+
+        [DataMember(Name = "v")]
+        public List<decimal> Volume { get; set; }
+    }
+}
